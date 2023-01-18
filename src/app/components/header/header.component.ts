6d822d1429e8f518504781644a3add2df2de2418
Aleksandr <aleksandr_dovgiyy@mail.ru>
2023-01-18 23:01:27 +0300

    Сделал шапку сайта

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {}
